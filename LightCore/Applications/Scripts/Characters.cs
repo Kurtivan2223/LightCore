e1c219acb23b1df3ddfab622e141d6c63e5a3af2
Kurtivan2223 <105442868+Kurtivan2223@users.noreply.github.com>
2024-02-19 22:42:25 +0800

    Added OnlinePlayerCharacterData

--- LightCore/Applications/Scripts/Characters.cs
+++ LightCore/Applications/Scripts/Characters.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using Dapper;
 
 namespace LightCore.Applications.Scripts
 {
@@ -92,8 +93,87 @@ namespace LightCore.Applications.Scripts
 			}
 		}
 #endregion
+
+#region OnlinePlayerCharacterData Operation
+
+		/// <summary>
+		/// Retrieves a list of online player character data from the characters database.
+		/// </summary>
+		/// <returns>
+		/// A list of <see cref="OnlinePlayerCharacterData"/> representing online player characters.
+		/// If no online players are found, the method returns an empty list.
+		/// </returns>
+		public static List<OnlinePlayerCharacterData> GetOnlinePlayer()
+		{
+			// Create a temporary list to store the retrieved data
+			List<OnlinePlayerCharacterData> temp = new List<OnlinePlayerCharacterData>();
+
+			// Establish a connection to the characters database
+			using (MySqlConnection connection = Database.GetConnection(ConnectionStrings.characters))
+			{
+				// Open the database connection
+				Database.Open(connection);
+
+				// Query online player character data from the characters table
+				var data = connection.Query<OnlinePlayerCharacterData>("SELECT name, race, class, gender, level FROM `characters` WHERE online = 1 ORDER BY level DESC LIMIT 49");
+
+				// Check if data is not null and contains any elements
+				if (data != null && data.Any())
+					temp.AddRange(data); // Add the retrieved data to the temporary list
+
+				// Close the database connection
+				Database.Close(connection);
+			}
+
+#pragma warning disable CS8603 // Possible null reference return.
+#if DEBUG
+			return temp.Count > 0 ? temp : null;
+#else
+			// Return the temporary list of online player character data
+			return temp;
+#endif
+#pragma warning restore CS8603 // Possible null reference return.
+		}
+
+#endregion
+
 #endregion
 	}
+
+	#region OnlinePlayerCharacterData
+	/// <summary>
+	/// Class representing online player character data.
+	/// </summary>
+	public class OnlinePlayerCharacterData : Characters
+	{
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+		/// <summary>
+		/// Gets or sets the name of the online player character.
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the race ID of the online player character.
+		/// </summary>
+		public int Race { get; set; }
+
+		/// <summary>
+		/// Gets or sets the class ID of the online player character.
+		/// </summary>
+		public int Class { get; set; }
+
+		/// <summary>
+		/// Gets or sets the gender ID of the online player character.
+		/// </summary>
+		public int Gender { get; set; }
+
+		/// <summary>
+		/// Gets or sets the level of the online player character.
+		/// </summary>
+		public int Level { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+	}
+#endregion
 
 #region CardPvPData
 	/// <summary>
